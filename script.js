2dfdbde3ce661cf3ddbe0fd1d9dabc68268cf187
TalkLounge <talklounge@yahoo.de>
2022-01-21 16:57:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // ==UserScript==
-// @name         tellonym.me guest
-// @version      1.0.0
-// @description  Load tellonym.me Tells without Login
-// @author       TalkLounge (https://github.com/TalkLounge)
-// @namespace    https://github.com/TalkLounge/tellonym.me-guest
-// @license      MIT
-// @match        https://tellonym.me/*
-// @require      https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js
-// @require      https://cdn.jsdelivr.net/npm/axios@0.24.0/dist/axios.min.js
-// @require      https://cdn.jsdelivr.net/npm/luxon@2.3.0/build/global/luxon.min.js
-// @grant        none
+// @name            Tellonym.me Guest
+// @name:de         Tellonym.me Gast
+// @version         1.0.1
+// @description     Load tellonym.me Tells without Login
+// @description:de  Lade tellonym.me Tells ohne Anmeldung
+// @author          TalkLounge (https://github.com/TalkLounge)
+// @namespace       https://github.com/TalkLounge/tellonym.me-guest
+// @license         MIT
+// @match           https://tellonym.me/*
+// @require         https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js
+// @require         https://cdn.jsdelivr.net/npm/axios@0.24.0/dist/axios.min.js
+// @require         https://cdn.jsdelivr.net/npm/luxon@2.3.0/build/global/luxon.min.js
+// @grant           none
 // ==/UserScript==
 
 (function () {
@@ -36,16 +38,32 @@
             }
 
             var $html = $(li.prop("outerHTML"));
+            $html.find("span[italic=false]").remove(); // Remove asked user
             $html.find("span").eq(0).text(answers[i].tell); // Question
             $html.find("span").eq(1).text(answers[i].answer); // Answer
             $html.find("[italic='false']").parent().parent().children().eq(1).text(luxon.DateTime.fromISO(answers[i].createdAt).toRelative()); // Time
+
+            if (!$html.find("div[data-radium=true]").last().find("svg").length) { // Likes
+                $html.find("div[data-radium=true]").last().text(answers[i].likesCount); // Set likes when in template
+            } else if (answers[i].likesCount) {
+                $html.find("div[data-radium=true]").last().parent().append($("<div>").text(answers[i].likesCount).css("padding-left", "5px").css("font-size", "14px")); // Set likes when was liked
+            }
+
+            for (var j = 0; j < answers[i].media.length; j++) { // Media
+                if (answers[i].media[j].type != 0) { // Only Pictures
+                    continue;
+                }
+                $html.find("span").eq(1).append($("<br>"));
+                $html.find("span").eq(1).append($("<a>").attr("href", answers[i].media[j].url).attr("target", "_blank"));
+                $html.find("span").eq(1).find("a").append($("<img>").attr("src", answers[i].media[j].thumbUrl));
+            }
+
             ul.append($html);
         }
     }
 
     async function scroll() {
-        const scrollbar = ul.parent();
-        if (scrollbar.prop("scrollTop") / scrollbar.prop("scrollHeight") > 0.75) { // Load more tells when scrolled down
+        if (ul.find(":nth-last-child(10)")[0].getBoundingClientRect().top <= document.body.clientHeight) { // Load more tells when scrolled down
             pos += 25;
             if (await loadTells()) { // End of tells reached
                 isProfilePage = false;
@@ -69,7 +87,7 @@
 
         lastUrl = window.location.href;
 
-        var localUl = $("span[data-radium=true]").first().parent().parent().parent().parent().parent().parent().parent().parent().parent();
+        var localUl = $("img[alt=avatar]").eq(2).parent().parent().parent().parent().parent().parent().parent().parent();
         const localLi = localUl.children().eq(1);
 
         if (!localLi.length) { // Page not loaded completely
